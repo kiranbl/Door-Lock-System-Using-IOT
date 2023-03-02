e95c135d27b8c55e88450fe2a4fc147d4be64f99
Kiran B L <57253045+kiranbl@users.noreply.github.com>
2023-03-02 21:09:45 +0000

    Update sms.py

--- Face Regognition and Raspberry PI python code/cv/sms.py
+++ Face Regognition and Raspberry PI python code/cv/sms.py
@@ -6,8 +6,8 @@ from twilio.rest import Client
 def smssend():
 # ------------------------------------------------------------------------------
 # Configurations
-    account_sid = "AC6c84705f8670736bed69983a15166cb7"
-    auth_token = "c16cefc4ffd1719d2721c789a93500aa"
+    account_sid = ""
+    auth_token = ""
 
     # ------------------------------------------------------------------------------
     # Setting the client with the configurations
@@ -16,8 +16,8 @@ def smssend():
     # ------------------------------------------------------------------------------
     # Creating the message
     client.messages.create(
-        to="+91 87928 95368",
-        from_="+1 951 457 5054",
+        to="",
+        from_="",
         body="Someone is at the door... plz follow this link to allow https://iot-webapplication.herokuapp.com"
     )
     
